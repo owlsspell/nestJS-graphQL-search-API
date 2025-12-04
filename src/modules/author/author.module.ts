09c68d8e43aa1c6a13b569ebd0afe429b0cad931
owlsspell <arhipowael@gmail.com>
2025-12-04 20:00:02 +0200

    Refactor author module and implement DataLoader for batch author fetching

--- src/modules/author/author.module.ts
+++ src/modules/author/author.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Author } from './author.entity';
+import { AuthorsRepository } from './authors.repository';
+import { AuthorsLoader } from './authors.loader';
 
-@Module({})
-export class AuthorModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Author])],
+  providers: [AuthorsRepository, AuthorsLoader],
+  exports: [AuthorsRepository, AuthorsLoader],
+})
+export class AuthorsModule {}
